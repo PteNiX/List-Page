46c76af3703c84d12a646bc31bff0cad7c19a753
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-09-17 22:41:48 +0300

    init: project 2.2 List page feat: *add tabs       *add table       *add information about users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule
+ 
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
