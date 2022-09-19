8f5a305ff68034357a0fdf106cc006dc7bf7dc0d
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-09-19 03:57:01 +0300

    feat: add path in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'navigator', component: AppComponent},
+  {path: 'navigator?tab=0', component: AppComponent},
+  {path: "navigator?tab=1", component: AppComponent},
+  {path: "navigator?tab=2", component: AppComponent},
+  {path: "navigator?tab=3", component: AppComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
