aed5c5a2763787704f9f4ebf3f4ba78b11d95e01
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-11-08 20:09:06 +0300

    refactor: *add prettier and eslint           *create new component listcomponent and migrate           data from appcomponent           *fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [{ path: 'navigator', component: AppComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'navigator' },
+  { path: 'navigator', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListComponent],
   imports: [BrowserModule, AppRoutingModule, MatTableModule, MatTabsModule],
   providers: [],
   bootstrap: [AppComponent],
